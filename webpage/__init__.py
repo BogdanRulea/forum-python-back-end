8198ffdbf03364a7a0910f4a145aee2fa19fc225
Luckkyf3r <bogdan.rulea@yahoo.com>
2021-11-14 15:41:31 +0200

    webhosting update.2

--- webpage/__init__.py
+++ webpage/__init__.py
@@ -24,8 +24,8 @@ def create_app():
 
     from .models import User, Post, Comment, Like
 
-    create_database(app)
-    print("Database created!")
+    #create_database(app)
+    #print("Database created!")
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
@@ -38,5 +38,4 @@ def create_app():
     return app
 
 def create_database(app):
-    if not path.exists("webpage/database.db"):
-        db.create_all(app=app)
+    db.create_all(app=app)
